cb3f29ec2d00f81a356f8e0bea8dbdf0a9f51d93
Lucas Alves <64313623+Lucas7Alves@users.noreply.github.com>
2024-08-02 09:36:26 -0300

    Set<T>

--- Generics/HashCodeEquals/src/App.java
+++ Generics/HashCodeEquals/src/App.java
@@ -6,7 +6,7 @@ public class App {
         Client c1 = new Client("Maria", "maria@gmail.com");
         Client c2 = new Client("Alex", "alex@hotmail.com");
 
-        //hasCode não é 100% seguro, mas é rápido
+        //hashCode não é 100% seguro, mas é rápido
         System.out.println(c1.hashCode());
         System.out.println(c2.hashCode());
         //equals não é rápido, mas é 100% seguro
